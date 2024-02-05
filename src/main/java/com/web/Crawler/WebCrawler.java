f862b4302b70c591a603d03312aa98980dbd953a
wlemfWKd <dlwldn2424@naver.com>
2024-02-05 11:01:44 +0900

    test 택

--- src/main/java/com/web/Crawler/WebCrawler.java
+++ src/main/java/com/web/Crawler/WebCrawler.java
@@ -33,10 +33,32 @@ public class WebCrawler {
             Document document2 = Jsoup.connect(url2).get();
             Elements titleElements2 = document2.select(".common_table01:first-child .list_tit");
             Elements contentElements2 = document2.select(".common_table01:first-child .list_cont");
+            
+            // 세 번째 URL
+            String url3= "https://www.youthcenter.go.kr/youngPlcyUnif/youngPlcyUnifDtl.do?pageIndex=1&frameYn=&bizId=R2024010218423&dtlOpenYn=&plcyTpOpenTy=&plcyCmprInfo=&srchWord=&chargerOrgCdAll=&srchAge=&trgtJynEmp=&trgtJynEmp=&srchSortOrder=1&pageUnit=12";
+            Document document3 = Jsoup.connect(url3).get();
+            Elements titleElements3 = document3.select(".common_table01:first-child .list_tit");
+            Elements contentElements3 = document3.select(".common_table01:first-child .list_cont");
+            
+            // 네 번째 URL
+            String url4= "https://www.youthcenter.go.kr/youngPlcyUnif/youngPlcyUnifDtl.do?pageIndex=1&frameYn=&bizId=R2024011018628&dtlOpenYn=&plcyTpOpenTy=&plcyCmprInfo=&srchWord=&chargerOrgCdAll=&srchAge=&trgtJynEmp=&trgtJynEmp=&srchSortOrder=1&pageUnit=12";
+            Document document4 = Jsoup.connect(url4).get();
+            Elements titleElements4 = document4.select(".common_table01:first-child .list_tit");
+            Elements contentElements4 = document4.select(".common_table01:first-child .list_cont");
+            
+            // 다섯 번째 URL
+            String url5= "https://www.youthcenter.go.kr/youngPlcyUnif/youngPlcyUnifDtl.do?pageIndex=1&frameYn=&bizId=R2024010518524&dtlOpenYn=&plcyTpOpenTy=&plcyCmprInfo=&srchWord=&chargerOrgCdAll=&srchAge=&trgtJynEmp=&trgtJynEmp=&srchSortOrder=1&pageUnit=12";
+            Document document5 = Jsoup.connect(url5).get();
+            Elements titleElements5 = document5.select(".common_table01:first-child .list_tit");
+            Elements contentElements5 = document5.select(".common_table01:first-child .list_cont");
 
             // 크롤링 결과를 담을 Map
             Map<String, String> resultMap1 = new HashMap<>();
             Map<String, String> resultMap2 = new HashMap<>();
+            Map<String, String> resultMap3 = new HashMap<>();
+            Map<String, String> resultMap4 = new HashMap<>();
+            Map<String, String> resultMap5 = new HashMap<>();
+            
 
             // 첫 번째 URL에서 제목과 내용을 Map에 추가
             for (int i = 0; i < titleElements1.size(); i++) {
@@ -53,10 +75,37 @@ public class WebCrawler {
                 resultMap2.put("li_title"+i, title);
                 resultMap2.put("li_cont"+i, content);
             }
+            
+            // 세 번째 URL에서 제목과 내용을 Map에 추가
+            for (int i = 0; i < titleElements3.size(); i++) {
+                String title = titleElements3.get(i).text();
+                String content = contentElements3.get(i).text();
+                resultMap3.put("li_title"+i, title);
+                resultMap3.put("li_cont"+i, content);
+            }
+            
+            // 네 번째 URL에서 제목과 내용을 Map에 추가
+            for (int i = 0; i < titleElements4.size(); i++) {
+                String title = titleElements4.get(i).text();
+                String content = contentElements4.get(i).text();
+                resultMap4.put("li_title"+i, title);
+                resultMap4.put("li_cont"+i, content);
+            }
+            
+            // 다섯 번째 URL에서 제목과 내용을 Map에 추가
+            for (int i = 0; i < titleElements5.size(); i++) {
+                String title = titleElements5.get(i).text();
+                String content = contentElements5.get(i).text();
+                resultMap5.put("li_title"+i, title);
+                resultMap5.put("li_cont"+i, content);
+            }
 
             // 최종 결과를 응답에 담음
             response.put("section1", resultMap1);
             response.put("section2", resultMap2);
+            response.put("section3", resultMap3);
+            response.put("section4", resultMap4);
+            response.put("section5", resultMap5);
 
         } catch (IOException e) {
             e.printStackTrace();
